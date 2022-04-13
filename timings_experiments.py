b6135793d8408508b7023eeb97eb3e9a434cb324
Mederic Fourmy <mederic.fourmy@gmail.com>
2022-04-13 17:44:31 +0200

    Check the influence of KLT pyramid level

--- timings_experiments.py
+++ timings_experiments.py
@@ -8,6 +8,8 @@ import matplotlib.pyplot as plt
 RUN_FILE = './main'
 PARAMS_MOD = 'params_mod.yaml'
 RESULT_FILE = 'build/result_tracking_timings.csv'
+STANDARD_COLORS = ['b','g','r','c','m','y','k']
+
 
 os.nice(1)
 
@@ -61,8 +63,8 @@ plt.grid()
 # experiment with tracking patch size
 patch_sizes = [9,13,17,21]
 plt.figure()
-plt.title('klt patch size trial')
-for ps in patch_sizes:
+plt.title('klt_patch_size trial')
+for i, ps in enumerate(patch_sizes):
     params['klt_patch_size'] = ps
 
     with open(PARAMS_MOD, 'w') as f:
@@ -72,8 +74,34 @@ for ps in patch_sizes:
     dt_detect_arr = df['dt_detect'].to_numpy()
     dt_track_arr = df['dt_track'].to_numpy()
     nb_to_tracks_arr = df['nb_to_tracks'].to_numpy()
-    plt.plot(iters_tracking, dt_track_arr, '.', label=str(ps))
+    plt.plot(nb_to_tracks_arr, dt_track_arr, STANDARD_COLORS[i]+'.', label=str(ps))
 
+plt.xlabel('# features tracked')
+plt.ylabel('time (s)')
 plt.legend()
+
+
+params['klt_patch_size'] = 15
+# experiment with tracking patch size
+pyramid_levels = [1,2,3,4]
+colors = ['r', 'r', ]
+plt.figure()
+plt.title('nlevels_pyramids_klt trial')
+for i, pl in enumerate(pyramid_levels):
+    params['nlevels_pyramids_klt'] = pl
+
+    with open(PARAMS_MOD, 'w') as f:
+        yaml.dump(params, f)
+    df = run_sequence(RUN_FILE, PARAMS_MOD, RESULT_FILE)
+    iters_tracking = df.index.to_numpy() 
+    dt_detect_arr = df['dt_detect'].to_numpy()
+    dt_track_arr = df['dt_track'].to_numpy()
+    nb_to_tracks_arr = df['nb_to_tracks'].to_numpy()
+    plt.plot(nb_to_tracks_arr, dt_track_arr, STANDARD_COLORS[i]+'.', label=str(pl))
+
+plt.xlabel('# features tracked')
+plt.ylabel('time (s)')
+plt.legend()
+
 
 plt.show()
